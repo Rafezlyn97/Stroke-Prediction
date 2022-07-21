ead3748abba5db2ba9807f04eecdec47d6218670
Rafezlyn97 <101541294+Rafezlyn97@users.noreply.github.com>
2022-07-21 16:40:00 +0800

    Update Stroke.py

--- Stroke.py
+++ Stroke.py
@@ -7,3 +7,20 @@ st.title("Stroke Prediction Dataset")
 st.header("Data collection")
 data = pd.read_csv('https://raw.githubusercontent.com/Rafezlyn97/Stroke-Prediction/main/healthcare-dataset-stroke-data.csv')
 st.write(pd.DataFrame(data))
+
+Y_name = st.sidebar.selectedbox('Select Label/Y', sorted(data))
+X_name = st.sidebar.multiselect('Select Predictors/X', sorted(data), default = sorted(data)[1], help = 'Can Select more than One')
+
+y = data.loc[:,Y_name]
+x = data.loc[:,X_name]
+X1 = x.select_dtypes(include = ['object']
+X2 = x.select_dtypes(exclude = ['object']
+
+if sorted(X1) != []:
+  X1 = X1.apply(LabelEncoder().fit_transform)
+  X = pd.concat([X2,X1], axis = 1)
+
+y = LabelEncoder().fit_transform(y)
+
+
+classifier_name = st.sidebar.selectedbox( 'Select Classifier', ('KKN', 'SVM', 'Random Forest'))
